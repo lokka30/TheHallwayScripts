0d1771d8d5016380987d6b21411172ca5ea1f8f0
lokka30 <59464084+lokka30@users.noreply.github.com>
2021-04-03 09:23:35 +0800

    Add files via upload

---
+++ THVersionBlocker.py
@@ -0,0 +1,36 @@
+"""
+THVersionBlocker (v1)
+
+Description:
+    Created for server 'The Hallway', this script
+    kicks players who have been detected as using
+    OpenSpades 0.0.12b, which lacks horizontal
+    recoil for SMGs.
+
+License: MIT License
+Author: lokka30
+More information: More information: https://github.com/lokka30/TheHallwayScripts
+"""
+
+def apply_script(protocol, connection, config):
+
+    class VersionBlockerProtocol(protocol):
+        def on_login(self, name):
+            full = self.client_string
+            
+            if full is None or full == "":
+                return self, name
+            
+            for players in self.protocol.players.values():
+                players.send_chat(name + " joined with client '" + full + "'.")
+            
+            split = full.split(" ")
+            client = split[0]
+            version = split[1].substring(0)
+            
+            if client == "OpenSpades" and version == "0.0.12b":
+                reactor.callLater(0.5, self.kick, "OpenSpades 0.0.12b is prohibited on this server.", False)
+            
+            return self, name
+
+    return VersionBlockerProtocol, connection
